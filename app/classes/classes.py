96f277540bef107503edb4896f30be91ba2961ac
ywang <marcowang2008@yahoo.com>
2017-06-02 01:20:45 +0800

    add template for teacher and class services

--- app/classes/classes.py
+++ app/classes/classes.py
@@ -4,17 +4,19 @@ from app.classes import classes
 from app.database import db
 import json
 
-class Teacher(db.Document):
-    name = db.StringField()
 
-class Book(db.Document):
+
+class Class(db.Document):
     title = db.StringField()
-    author = db.DocumentField(Teacher)
-    year = db.IntField()
+    teacher = db.DocumentField(Teacher)
+    studentNum = db.IntField()
 
+@classes.route('/api/v1/class', methods=['GET'])
+def getClass():
+    pass
 
-@classes.route('/add', methods=['POST'])
-def apidemo():
+@classes.route('/api/v1/class', methods=['POST'])
+def addClass():
     """一个返回JSON数据接口的设计示例"""
 
     data = json.loads(request.data)
@@ -25,3 +27,9 @@ def apidemo():
     jsonResponse = dict(errCode="1", errMsg="操作成功！")
     response = jsonify(jsonResponse)
     return response
+
+
+
+@classes.route('/api/v1/class', methods=['PUT'])
+def editClass():
+    pass
--- app/users/teachers.py
+++ app/users/teachers.py
@@ -1,12 +1,27 @@
 #-*- coding: UTF-8 -*-
 from flask import request, jsonify
 from app.users import users
+from app.database import db
 
 
-@users.route('/add', methods=['GET', 'POST'])
-def apidemo():
+class Teacher(db.Document):
+    name = db.StringField()
+    profile = db.StringField()
+    img = db.StringField()
+
+
+@users.route('/api/v1/teacher', methods=['GET'])
+def getTeacher():
     """一个返回JSON数据接口的设计示例"""
+    pass
 
-    jsonResponse = dict(errCode="1", errMsg="操作成功！")
-    response = jsonify(jsonResponse)
-    return response
+@users.route('/api/v1/teacher', methods=['POST'])
+def addTeacher():
+    """一个返回JSON数据接口的设计示例"""
+    pass
+
+
+@users.route('/api/v1/teacher', methods=['PUT'])
+def editTeacher():
+    """一个返回JSON数据接口的设计示例"""
+    pass
