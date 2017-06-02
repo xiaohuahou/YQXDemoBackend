a9b2c2db94ceb8cd99df3d18fa0a60c1af8cacd5
Anthony Huang <anthuang@umich.edu>
2017-06-01 22:28:47 -0700

    add a demo restful call to write teacher info into mongodb

--- app/users/teachers.py
+++ app/users/teachers.py
@@ -1,19 +1,29 @@
 #-*- coding: UTF-8 -*-
-from flask import request, jsonify
+from flask import request
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from app.database import db
 from app.users import users
+
 
 @users.route('/teachers', methods=['GET'])
 def getTeacher():
-    """一个返回JSON数据接口的设计示例"""
-    pass
+    teachers = db.teachers.find()
+    return dumps(teachers)
+
 
 @users.route('/teachers', methods=['POST'])
 def addTeacher():
-    """一个返回JSON数据接口的设计示例"""
-    pass
+    name = request.args.get('name', '')
+    db.teachers.insert_one({
+        'name': name
+    })
+    return "Success"
 
 
-@users.route('/teachers', methods=['PUT'])
-def editTeacher():
-    """一个返回JSON数据接口的设计示例"""
-    pass
+@users.route('/teachers/<string:teacher_id>', methods=['PUT'])
+def editTeacher(teacher_id):
+    name = request.args.get('name', '')
+    db.teachers.update_one({'_id': ObjectId(teacher_id)},
+                           {'$set': {'name': name}})
+    return "Success"
